7377367e2c5208b5b4a068b053dbf8641192d4a7
Estifanos <estifanosbeyene21@gmail.com>
2022-10-03 18:00:40 -0400

    feat: change user model

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -4,7 +4,8 @@ import log from "./logger";
 const connect = async () => {
   try {
     const prisma = new PrismaClient();
-    log.info(`DB connected: ${prisma}`);
+    log.info(`DB is working`);
+    prisma.$disconnect();
   } catch (error) {
     log.error("Could not connect to db");
     process.exit(1);
