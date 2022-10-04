9e8c85b711e86ce3e6f83d610f40d2e4daee2584
Estifanos <estifanosbeyene21@gmail.com>
2022-10-04 16:41:11 -0400

    feat(user.service): add func for validating pw

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 import config from "config";
+import { omit } from "lodash";
 import { CreateUserInput } from "../schema/user.schema";
 
 export const createUser = async ({ body }: CreateUserInput) => {
@@ -14,9 +15,33 @@ export const createUser = async ({ body }: CreateUserInput) => {
     data: {
       email: body.email,
       name: body.name,
-      password: hash.toString()
+      password: hash.toString(),
     },
   });
 
   return user;
+};
+
+export const validatePassword = async ({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) => {
+  const prisma = new PrismaClient();
+
+  // find user with email
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  // return false is no user or no match, else return user
+  return !user
+    ? false
+    : !(await bcrypt.compare(password, user.password).catch((_) => false))
+    ? false
+    : omit(user, ["password"]);
 };
