f1e6194a8ab7681a7b683e303d470b4fd126cd18
Estifanos <estifanosbeyene21@gmail.com>
2022-10-04 11:21:50 -0400

    fix: add hashed password to db

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,26 +1,20 @@
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import config from "config";
 import { CreateUserInput } from "../schema/user.schema";
 
 export const createUser = async ({ body }: CreateUserInput) => {
+
+  // create hash
+  const salt = await bcrypt.genSalt(config.get<number>("saltWorkFactor"));
+  const hash = bcrypt.hashSync(body.password, salt);
+
   const prisma = new PrismaClient();
-
-  // middleware for hashing pw
-  prisma.$use(async (params, next) => {
-    if (params.model == "User" && params.action == "create") {
-      const salt = await bcrypt.genSalt(config.get<number>("saltWorkFactor"));
-      const hash = bcrypt.hashSync(params.args.password, salt);
-      params.args.data.password = hash;
-    }
-    return next;
-  });
-
   const user = await prisma.user.create({
     data: {
       email: body.email,
       name: body.name,
-      password: body.password,
+      password: hash // set pw to hash
     },
   });
 
