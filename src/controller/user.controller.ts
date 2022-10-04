a649862a34d8e9f78ea1e83355d3e705fff62ae2
Estifanos <estifanosbeyene21@gmail.com>
2022-10-04 11:22:07 -0400

    fix: change response to successful user create

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -3,9 +3,13 @@ import { CreateUserInput } from "../schema/user.schema";
 import { createUser } from "../service/user.service";
 import log from "../utils/logger";
 
-const createUserHandler = async (req: Request<{}, {}, CreateUserInput['body']>, res: Response) => {
+const createUserHandler = async (
+  req: Request<{}, {}, CreateUserInput["body"]>,
+  res: Response
+) => {
   try {
-    return await createUser(req);
+    const user = await createUser(req);
+    return res.status(200).json({ user: user });
   } catch (error: any) {
     log.error(error);
     return res.status(409).send(error.message); // conflict
