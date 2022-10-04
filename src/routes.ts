0b1e7f7056132734c61310d243825a0370f51212
Estifanos <estifanosbeyene21@gmail.com>
2022-10-04 06:21:17 -0400

    feat: add create user route

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
 import { Express, Request, Response } from "express";
+import { createUserHandler } from "./controller/user.controller";
+import validate from "./middleware/validateResource";
+
 const routes = (app: Express) => {
   app.get("/healthcheck", (req: Request, res: Response) => {
     return res.sendStatus(200);
   });
+  app.post("/api/users", validate, createUserHandler);
 };
 
 export default routes;
