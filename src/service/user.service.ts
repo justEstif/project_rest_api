4ef738f970c1b66a3138b72962250d20aeb5a5d5
Estifanos <estifanosbeyene21@gmail.com>
2022-10-04 06:20:18 -0400

    feat: add user service

---
+++ src/service/user.service.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import config from "config";
+import { CreateUserInput } from "../schema/user.schema";
+
+export const createUser = async ({ body }: CreateUserInput) => {
+  const prisma = new PrismaClient();
+  // middleware for hashing pw
+  prisma.$use(async (params, next) => {
+    if (params.model == "User" && params.action == "create") {
+      const salt = await bcrypt.genSalt(config.get<number>("saltWorkFactor"));
+      const hash = bcrypt.hashSync(params.args.password, salt);
+      params.args.data.password = hash;
+    }
+    return next;
+  });
+
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      name: body.name,
+      password: body.password,
+    },
+  });
+  return user;
+};
