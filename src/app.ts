4b53851360bd3bbdeb25fbccf1ff939ad935cf9f
Estifanos <estifanosbeyene21@gmail.com>
2022-10-04 11:21:30 -0400

    feat: add body parser to express

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { json, urlencoded } from "express";
 import config from "config";
 import log from "./utils/logger";
 import routes from "./routes";
@@ -6,9 +6,11 @@ import connect from "./utils/connect";
 
 const app = express();
 const port = config.get<number>("port");
+app.use(json()); // body parser
+app.use(urlencoded({ extended: false }));
 
 app.listen(port, async () => {
-  await connect()
+  await connect();
   log.info(`App is running at http://localhost:${port}`);
   routes(app);
 });
