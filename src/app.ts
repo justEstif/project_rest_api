13eaae9e9566dd990e48b4b5465f7d5d3320f7db
Estifanos <estifanosbeyene21@gmail.com>
2022-10-05 03:37:12 -0400

    feat: remove unused db connection logger

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express, { json, urlencoded } from "express";
 import config from "config";
 import log from "./utils/logger";
 import routes from "./routes";
-import connect from "./utils/connect";
 
 const app = express();
 const port = config.get<number>("port");
@@ -10,7 +9,6 @@ app.use(json()); // body parser
 app.use(urlencoded({ extended: false }));
 
 app.listen(port, async () => {
-  await connect();
   log.info(`App is running at http://localhost:${port}`);
   routes(app);
 });
