9a581c3b6ef3e9d46c2bc0c8bd81784f0a0073f5
Estifanos <estifanosbeyene21@gmail.com>
2022-10-05 11:26:36 -0400

    feat: add conditional to handle expired tokens

--- src/middleware/deserializeUser.ts
+++ src/middleware/deserializeUser.ts
@@ -1,23 +1,35 @@
 import { RequestHandler } from "express";
 import { get } from "lodash";
+import { reIssueAccessToken } from "../service/session.service";
 import { verifyJwt } from "../utils/jwt";
 
-const deserializeUser: RequestHandler = (req, res, next) => {
-  const accessToken = get(req.headers, "authorization", null)?.replace(
+const deserializeUser: RequestHandler = async (req, res, next) => {
+  const accessToken = get(req.headers, "authorization", "")?.replace(
     /^Bearer\s/,
     ""
   );
-  if (accessToken) {
-    const { decoded } = verifyJwt(accessToken);
-    if (decoded) {
-      res.locals.user = decoded;
-      return next();
-    } else {
-      return next();
-    }
-  } else {
+
+  const refreshToken = get(req.headers, "x-refresh", null);
+
+  if (!accessToken) return next();
+
+  const { decoded, expired } = verifyJwt(accessToken);
+
+  if (decoded) {
+    res.locals.user = decoded;
     return next();
   }
+
+  if (expired && typeof refreshToken === "string") {
+    const newAccessToken = await reIssueAccessToken({ refreshToken });
+    if (newAccessToken) {
+      res.setHeader("x-access-token", newAccessToken);
+      const result = verifyJwt(newAccessToken);
+      res.locals.user = result.decoded;
+    }
+  }
+
+  return next();
 };
 
 export default deserializeUser;
