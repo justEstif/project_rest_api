50609004f33b5b08d48cb1ea45aeef0b93640b5b
Estifanos <estifanosbeyene21@gmail.com>
2022-10-05 05:54:04 -0400

    feat: add requireUser middleware to get session route

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,22 @@
 import { Express, Request, Response } from "express";
-import { createUserSession } from "./controller/session.controller";
+import {
+  createUserSession,
+  getUserSessionsHandler,
+} from "./controller/session.controller";
 import { createUserHandler } from "./controller/user.controller";
+import requireUser from "./middleware/requireUser";
 import validate from "./middleware/validateResource";
 import { createSessionSchema } from "./schema/session.schema";
 import { createUserSchema } from "./schema/user.schema";
 
 const routes = (app: Express) => {
-  app.get("/healthcheck", (req: Request, res: Response) => {
-    return res.sendStatus(200);
-  });
+  app.get("/healthcheck", (_: Request, res: Response) => res.sendStatus(200));
+
   app.post("/api/users", validate(createUserSchema), createUserHandler);
+
   app.post("/api/sessions", validate(createSessionSchema), createUserSession);
+
+  app.get("/api/sessions", requireUser, getUserSessionsHandler);
 };
 
 export default routes;
