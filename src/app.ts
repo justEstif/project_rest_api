e79198217c470fa4420f45f4f05dcb99b0b47e81
Estifanos <estifanosbeyene21@gmail.com>
2022-10-05 05:49:43 -0400

    feat: add deserializeUser middleware to all routes

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,14 @@ import express, { json, urlencoded } from "express";
 import config from "config";
 import log from "./utils/logger";
 import routes from "./routes";
+import deserializeUser from "./middleware/deserializeUser";
 
 const app = express();
 const port = config.get<number>("port");
 app.use(json()); // body parser
 app.use(urlencoded({ extended: false }));
+
+app.use(deserializeUser);
 
 app.listen(port, async () => {
   log.info(`App is running at http://localhost:${port}`);
